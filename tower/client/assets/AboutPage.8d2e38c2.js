8a57cd6658e7af952abaf8310b7cb78d6317c71c
Scott Bickish <sbickish3@gmail.com>
2021-12-10 16:48:23 -0700

    update

---
+++ tower/client/assets/AboutPage.8d2e38c2.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.69f5e11a.js";import{o,b as s,d as t}from"./vendor.f20538c6.js";const a={},c={class:"about"},r=t("h1",null,"This is the about page",-1),n=[r];function _(d,i,p,f,u,h){return o(),s("div",c,n)}var b=e(a,[["render",_]]);export{b as default};
